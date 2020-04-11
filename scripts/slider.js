467f8b4d43222f24bf693473534426bd263ad9c5
Exucounter <exucounter777@gmail.com>
2020-04-12 01:00:22 +0300

    Fix slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -24,15 +24,3 @@ $('.client-feedback__slider').slick({
 
 let rightArrowServicesSlider = document.querySelector('.services-slider .slick-next');
 let leftArrowServicesSlider = document.querySelector('.services-slider .slick-prev');
-
-rightArrowServicesSlider.addEventListener('click', ()=>{
-    leftArrowServicesSlider.classList.add('active');
-    rightArrowServicesSlider.classList.remove('active');
-    rightArrowServicesSlider.classList.add('disabled');
-})
-
-leftArrowServicesSlider.addEventListener('click', ()=>{
-    rightArrowServicesSlider.classList.add('active');
-    leftArrowServicesSlider.classList.remove('active');
-    leftArrowServicesSlider.classList.add('disabled');
-})
