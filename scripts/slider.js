a2c996b004299522fc41019805726f30ae77b392
Exucounter <exucounter777@gmail.com>
2020-04-11 21:52:27 +0300

    media queries

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,7 +1,18 @@
 $('.services-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
-    infinite: false
+    infinite: false,
+    // responsive: [
+    //     {
+    //       breakpoint: 1700,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 3,
+    //         infinite: true,
+    //         dots: true
+    //       }
+    //     }
+    //   ]
 });
 
 $('.client-feedback__slider').slick({
